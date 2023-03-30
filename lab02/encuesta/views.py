639d295352f7427548a2dfb02f339ed2367cd4c1
rqueaArequipa <ruben.quea@tecsup.edu.pe>
2023-03-30 07:43:18 -0500

    Avance-Lab-02

---
+++ lab02/encuesta/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'titulo' : "Formulario",
+    }
+    return render(request, 'encuesta/formulario.html', context)
+
+def enviar(request):
+    context = {
+        'titulo' : 'Respuestas',
+        'nombre' : request.POST['nombre'],
+        'clave' : request.POST['password'],
+        'educacion' : request.POST['educacion'],
+        'nacionalidad' : request.POST['nacionalidad'],
+        'idiomas' : request.POST.getlist('idiomas'),
+        'correo' : request.POST['email'],
+        'website' : request.POST['sitioweb'],
+    }
+    return render(request, 'encuesta/respuesta.html', context)
